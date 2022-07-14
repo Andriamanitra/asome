40b2574c123f6f270a61deeec2658c2c6c7f9851
Andriamanitra <mikko77792@gmail.com>
2022-07-14 09:34:57 +0300

    fix heroku allowed url

--- asocial/settings.py
+++ asocial/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_APP_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") is not None
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "heroku.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
